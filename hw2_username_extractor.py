b534419b1a067e5a93926d0266b52b19d8eadd3f
jomart-gsu <jmartin191@gsu.edu>
2022-01-24 16:29:59 -0500

    initial commit

--- hw2_username_extractor.py
+++ hw2_username_extractor.py
@@ -1,5 +1,6 @@
 import os
 
+# HW2 submissions bulk downloaded from iCollege
 DIRECTORIES = [
     "Homework 2 - Development Environment Setup Download Jan 22, 2022 1029 AM",
     "Homework 2 - Development Environment Setup Download Jan 23, 2022 1117 AM",
@@ -15,6 +16,9 @@ def get_usernames_from_directory(dir):
             continue
         full_path = os.path.join(base_path, filename)
         with open(full_path) as f:
+            # File should just contain an html snippet containing a repo URL.
+            # This method of splitting the string is brittle but seems to work
+            # for our purposes.
             url = f.read().strip()
             if "github.com/" in url:
                 username = url.split("github.com/")[1].split("/")[0]
@@ -27,6 +31,9 @@ def get_usernames_from_directory(dir):
 
 
 if __name__ == "__main__":
+    """
+    Compile giant list of usernames, write each one to its own line in a file
+    """
     usernames = []
     for directory in DIRECTORIES:
         usernames.extend(get_usernames_from_directory(directory))
